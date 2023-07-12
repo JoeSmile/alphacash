fc6543c7659a6c295eecde365e0d269a4202d050
QinghuaMa <qinghua.ma@shopee.com>
2023-07-12 20:22:11 +0800

    chore: app自动更新的配置

--- app.config.js
+++ app.config.js
@@ -1,10 +1,54 @@
 module.exports = ({ config }) => ({
-  ...config,
+  name: "financeexpo",
+  slug: "financeexpo",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+    package: "com.alphacash.easy.credit.loan.paisa",
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+  scheme: "financeexpo",
+  runtimeVersion:
+    process.env.ENV === "dev"
+      ? {
+          policy: "sdkVersion",
+        }
+      : "0.0.2",
+  updates: {
+    fallbackToCacheTimeout: 0,
+    url: "https://u.expo.dev/462bceb9-15ba-4d76-bcad-d93ef54c4c31",
+  },
+  plugins: [
+    "expo-localization",
+    [
+      "expo-document-picker",
+      {
+        iCloudContainerEnvironment: "Production",
+      },
+    ],
+  ],
   extra: {
-    ENV: process.env.ENV || "dev",
+    ENV: process.env.ENV,
     eas: {
       // projectId: "c5ed18ff-989f-4bed-934b-ccce0c4d67f5", // for qinghua
-       projectId: "462bceb9-15ba-4d76-bcad-d93ef54c4c31", // for sunhao
+      projectId: "462bceb9-15ba-4d76-bcad-d93ef54c4c31", // for sunhao
     },
   },
 });
--- screens/Homepage/index.js
+++ screens/Homepage/index.js
@@ -1,34 +1,45 @@
 import { Pressable, StyleSheet } from "react-native";
 import { Text, View } from "../../components/Themed";
 import { useI18n, LocaleTypes } from "@hooks/useI18n";
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import { RFValue } from 'react-native-responsive-fontsize';
-import { Button, Toast, Provider } from '@ant-design/react-native';
-import { CompanyIntro } from './CompanyIntro';
-import { Quota } from './Quota';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { RFValue } from "react-native-responsive-fontsize";
+import { Button, Toast, Provider } from "@ant-design/react-native";
+import { CompanyIntro } from "./CompanyIntro";
+import { Quota } from "./Quota";
 
 export default function Homepage() {
   const { i18n, setLocale, locale } = useI18n();
 
   return (
-    <View style={styles.container}> 
-      <View style={{
-        top: 0,
-        position: "absolute",
-        backgroundColor: "#0825B8",
-        width: '100%',
-        height: hp(20),
-        zIndex: 0,
-      }}/>
+    <View style={styles.container}>
+      <View
+        style={{
+          top: 0,
+          position: "absolute",
+          backgroundColor: "#0825B8",
+          width: "100%",
+          height: hp(20),
+          zIndex: 0,
+        }}
+      />
       <CompanyIntro />
       <Quota />
       <Text style={styles.title}>{i18n.t("welcome")}</Text>
-      <Pressable onPress={() => setLocale(locale === LocaleTypes.en ? LocaleTypes.urdu : LocaleTypes.en)}>
+      <Pressable
+        onPress={() =>
+          setLocale(
+            locale === LocaleTypes.en ? LocaleTypes.urdu : LocaleTypes.en
+          )
+        }
+      >
         <Text style={styles.test}>switch language</Text>
       </Pressable>
-        <Button onPress={() => Toast.info('This is a toast tips')} color='black'>
-          Start111  
-        </Button>
+      <Button onPress={() => Toast.info("This is a toast tips")} color="black">
+        Start111
+      </Button>
     </View>
   );
 }
@@ -38,7 +49,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    position: 'relative',
+    position: "relative",
   },
   title: {
     fontSize: 20,
@@ -56,5 +67,5 @@ const styles = StyleSheet.create({
     height: hp(20),
     marginVertical: hp(1),
     textAlign: "center",
-  }
+  },
 });

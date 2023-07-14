db142231b69c2d6fd3c86550fc1133c3769125c3
joe <253482219@qq.com>
2023-07-14 15:53:01 +0800

    fix: add status placeholder

--- screens/Homepage/Quota.js
+++ screens/Homepage/Quota.js
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet } from "react-native";
+import { Image, StyleSheet } from "react-native";
 import { Text, View } from "../../components/Themed";
 import { useI18n, LocaleTypes } from "@hooks/useI18n";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
@@ -17,10 +17,19 @@ export function Quota() {
         justifyContent: 'center',
         alignItems: 'center'
       }}>
+        <Image source={require("@assets/bills/loan_tag_overdue.png")} 
+          style={{
+            position: 'absolute',
+            width: 102,
+            height: 73,
+            top: 0,
+            right: 0
+          }}/>
         <Text style={{
           fontSize: 16,
+
           color: '#0A233E',
-          marginTop: 15,
+          marginTop: 30,
           marginBottom: 15
         }}>Max Amount</Text>
         <Text style={{
@@ -32,7 +41,9 @@ export function Quota() {
       <FButton style={{
           borderWidth: 0, 
           backgroundColor: '#0825B8',
-          marginBottom: 30
+          marginBottom: 45,
+          marginRight: 15,
+          marginLeft: 15
         }}
         onPress={() => navigation.push('Apply')}
         title='Get Loan'
@@ -44,8 +55,8 @@ export function Quota() {
 
 const styles = StyleSheet.create({
   container: {
-   padding: 15,
    margin: 15,
+   paddingBottom: 15,
    borderRadius: 4,
    boxShadow: '1px 1px 3px 1px rgba(0, 0, 0, 0.1),1px 1px 2px 1px rgba(0, 0, 0, 0.06)'
   },

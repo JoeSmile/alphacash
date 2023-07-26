90d8be046617c089df6ee3071ade2250ac075f63
joe <253482219@qq.com>
2023-07-26 09:30:10 +0800

    feat: contact us style

--- screens/Account/screens/ContactUs.js
+++ screens/Account/screens/ContactUs.js
@@ -1,20 +1,106 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image, Pressable } from "react-native";
 import FList from "@components/FList";
+import * as Linking from "expo-linking"
+import { A } from "@expo/html-elements";
+
+const Item = (item) => {
+  return (
+    <View style={styles.item}>
+      <Image
+        source={item.leftIcon}
+        contentFit="cover"
+        transition={1000}
+        style={{
+          width: 25,
+          height: 25,
+          marginRight: 12,
+        }}
+      />
+        <Text style={{
+          fontSize: 16
+        }}>{item.title}</Text>
+    </View>
+  );
+};
+
 const data = [
   {
-    title: "在线客服(推荐)",
+    title: "在线客服",
+    leftIcon: require("@assets/images/mine_ic_online_service.png"),
+    leftItem: (item) => {
+      return (
+        <>
+          <A
+            href="https://google.com"
+            style={{
+              color: "#0825B8",
+              textDecorationLine: "none",
+              fontWeight: "bold",
+            }}
+          >
+            <View style={{
+              flexDirection: 'row',
+              alignItems:'center'
+            }}>
+              <Image
+                source={item.leftIcon}
+                contentFit="cover"
+                transition={1000}
+                style={{
+                  width: 25,
+                  height: 25,
+                  marginRight: 12,
+                }}
+              />
+              <Text style={{color: '#0A233E'}}>{item.title}</Text>
+              <Text style={{
+                color: '#8899AC',
+                marginLeft: 5
+              }}>Recommend</Text>
+            </View>
+          </A>
+        </>
+      );
+    },
   },
   {
     title: "联系邮箱： xxxx@gmail.com",
+    leftIcon: require("@assets/images/mine_ic_email.png"),
+    leftItem: Item,
   },
   {
     title: "联系电话： xxxxx",
+    tel: 'xxxx',
+    leftIcon: require("@assets/images/mine_ic_contact_number.png"),
     displayIcon: false,
     leftItem: (item) => {
       return (
         <>
-          <Text>{item.title}</Text>
-          <Text>拨打</Text>
+          <View style={{
+            flexDirection: 'row',
+            alignItems:'center'
+          }}>
+            <Image
+              source={item.leftIcon}
+              contentFit="cover"
+              transition={1000}
+              style={{
+                width: 25,
+                height: 25,
+                marginRight: 12,
+              }}
+            />
+            <Text>{item.title}</Text>
+          </View>
+          <Pressable onPress={() => {
+              //TODO: +区号
+              Linking.openURL(`tel: ${item.tel}`)
+            }}>
+              <Text style={{
+                color: '#0825B8',
+                fontSize: 16
+              }}>拨打</Text>
+          </Pressable>
         </>
       );
     },
@@ -23,12 +109,54 @@ const data = [
 
 export default function ContactUs() {
   return (
-    <View>
-      <FList data={data} />
-      <Text>
-        Key Executive For Loan Handling officer Name：xxxxxxx Conact
-        Email:XXXXXX@com
-      </Text>
+    <View style={{
+      backgroundColor: 'white',
+      height: '100%'
+    }}>
+      <View style={styles.container}>
+        <FList data={data} itemStyle={styles.FList}/>
+        <Text style={{
+          color: '#8899AC',
+          fontSize: 12,
+          lineHeight: 18
+        }}>
+          Key Executive For Loan Handling officer Name: xxxxxxx Contact
+          Email:XXXXXX@com
+        </Text>
+      </View>
     </View>
+   
   );
 }
+const styles = StyleSheet.create({
+  image: {
+    height: "100%",
+    width: "100%",
+  },
+  container: {
+    paddingVertical: 30,
+    paddingHorizontal: 20,
+    backgroundColor: "white",
+  },
+  itemsContainer: {
+    margin: 15,
+    transform: "translateY(-50px)",
+  },
+  item: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  FList: {
+    height: 60,
+    borderWidth: 1,
+    borderColor: '#C0C4D6',
+    backgroundColor: "white",
+    paddingHorizontal: 12,
+    paddingVertical: 12,
+    borderRadius: 4,
+  },
+  text: {
+    color: "white",
+  },
+});

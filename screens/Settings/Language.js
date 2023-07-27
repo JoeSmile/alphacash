8cecd018a8005695876ba5e3d1f716a9510db17f
joe <253482219@qq.com>
2023-07-27 13:43:37 +0800

    feat: language style

--- screens/Settings/Language.js
+++ screens/Settings/Language.js
@@ -1,6 +1,5 @@
-import { Text, Pressable, View, FlatList } from "react-native";
+import { Text, Pressable, View, FlatList, Image } from "react-native";
 import { useI18nStore } from "@store/useI18nStore";
-import { Ionicons } from "@expo/vector-icons";
 
 const data = [
   {
@@ -29,19 +28,37 @@ const Item = ({ locale, title }) => {
         style={{
           flexDirection: "row",
           justifyContent: "space-between",
+          height: 60,
+          borderWidth: 2,
+          borderColor: currentLocale == locale ? '#0825B8' : '#C0C4D6',
+          lineHeight: 60,
+          alignItems: 'center',
+          padding: 10,
+          borderRadius: 4,
+          marginTop: 10,
+          backgroundColor: 'white'
         }}
       >
-        <Text>{title}</Text>
-        {currentLocale == locale && (
-          <Ionicons name="checkmark" size={12} color="black" />
-        )}
+        <Text style={{
+          color: '#0A233E',
+          fontSize: 16
+        }}>{title}</Text>
+        <Image source={currentLocale == locale ? require("@assets/images/bank_card_radio_sel.png") :  require("@assets/images/unSelected.png")}
+            contentFit="cover"
+            transition={200}
+            style={{ width: 20, height: 20}} 
+          />
       </View>
     </Pressable>
   );
 };
 export const Language = () => {
   return (
-    <View>
+    <View style={{
+      padding: 20,
+      backgroundColor: 'white',
+      height: '100%'
+    }}>
       <FlatList
         data={data}
         renderItem={({ item }) => (
